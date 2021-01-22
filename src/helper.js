c02572e19930b0776b08d9764b676d5d4360d055
mac <mac@Roman-Timoshchuk.local>
2021-01-22 14:34:16 +0200

    create repo

--- src/helper.js
+++ src/helper.js
@@ -8,7 +8,7 @@ export function calculateWinner(squares) {
 		[2, 5, 8],
 		[0, 4, 8],
 		[2, 4, 6],
-	];
+	]
 	for (let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i]
 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
